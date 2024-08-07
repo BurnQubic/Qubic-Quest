5e7e8b30075730449b412ac4feba26cbe53f549c
real-venus <yamamotoyuki120@gmail.com>
2024-08-07 18:58:29 +0900

    fix: remove password

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -11,12 +11,14 @@ export class UsersService {
     return this.prisma.user.findUnique({ where: { email } });
   }
 
-  async createUser(email: string, password: string): Promise<User> {
-    const hashedPassword = await bcrypt.hash(password, 10);
+  async createUser(
+    uid: string,
+    email: string,
+  ): Promise<User> {
     return this.prisma.user.create({
       data: {
+        uid,
         email,
-        password: hashedPassword,
         username: email,
         walletAddress: "",
       },
