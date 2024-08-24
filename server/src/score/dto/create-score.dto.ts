77714944a073615be19b752202d476e25762872a
real-venus <yamamotoyuki120@gmail.com>
2024-08-24 11:51:04 +0900

    fix: hard coding, docker compose, puzzle controller improvement

--- server/src/score/dto/create-score.dto.ts
+++ server/src/score/dto/create-score.dto.ts
@@ -1,1 +1,11 @@
-export class CreateScoreDto {}
+import { IsNotEmpty, IsNumber } from "class-validator";
+
+export class CreateScoreDto {
+  @IsNotEmpty()
+  @IsNumber()
+  score: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  userId: number;
+}
