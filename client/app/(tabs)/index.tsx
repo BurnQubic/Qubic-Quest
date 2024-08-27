96532076867e0439f49780a8fb166b2d9ab8fec6
real-venus <yamamotoyuki120@gmail.com>
2024-08-27 16:48:39 +0900

    feat: main page

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,57 +1,130 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { StyleSheet, Platform, TouchableOpacity, Text } from "react-native";
 
 import { HelloWave } from "@/app/components/HelloWave";
 import ParallaxScrollView from "@/app/components/common/ParallaxScrollView";
-import { ThemedView } from "@/app/components/common/ThemedView";
-import { ThemedText } from "@/app/components/common/ThemedText";
 import GameScreen from "./game";
+import { ScrollView } from "react-native-gesture-handler";
+import { Image } from "expo-image";
+
+import candyImage from "@/assets/images/main/candy-game.jpg";
+import ghostImage from "@/assets/images/main/candy-game.jpg";
+import numbersImage from "@/assets/images/main/candy-game.jpg";
+import { View } from "@react-three/drei";
+
+const images = [
+  {
+    title: "Identical items",
+    category: "ATTENTION",
+    image: candyImage,
+  },
+  {
+    title: "Hidden ghosts",
+    category: "MEMORY",
+    image: ghostImage,
+  },
+  {
+    title: "Ancient numbers",
+    category: "ATTENTION",
+    image: numbersImage,
+  },
+];
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={<Image source={require("@/assets/images/partial-react-logo.png")} style={styles.reactLogo} />}
+      headerImage={<Image source={require("@/assets/images/partial-react-logo.png")} style={styles.logo} />}
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">{Platform.select({ ios: "cmd + d", android: "cmd + m" })}</ThemedText> to
-          open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>Tap the Explore tab to learn more about what's included in this starter app.</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <View style={styles.container}>
+        <Text style={styles.header}>Today</Text>
+        <Text style={styles.subtitle}>Your personal selection of exercises for different brain areas</Text>
+        <ScrollView>
+          <View style={styles.timelineContainer}>
+            {images.map((exercise, index) => (
+              <View key={index} style={styles.exerciseContainer}>
+                <View style={styles.circle} />
+                <TouchableOpacity style={styles.exercise}>
+                  <Image source={exercise.image} style={styles.image} />
+                  <View>
+                    <Text style={styles.title}>{exercise.title}</Text>
+                    <Text style={styles.category}>{exercise.category}</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </View>
+        </ScrollView>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Start playing</Text>
+        </TouchableOpacity>
+      </View>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 40,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 14,
+    marginBottom: 20,
+  },
+  timelineContainer: {
+    flexDirection: "column",
+  },
+  exerciseContainer: {
     flexDirection: "row",
     alignItems: "center",
-    gap: 8,
+    marginBottom: 20,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  circle: {
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    marginRight: 16,
+    alignSelf: "flex-start",
+    marginTop: 20,
   },
-  reactLogo: {
+  exercise: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+    borderRadius: 10,
+    flex: 1,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 16,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  category: {
+    fontSize: 12,
+  },
+  button: {
+    paddingVertical: 16,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 20,
+    backgroundColor: "#00aaff",
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  logo: {
     height: 178,
     width: 290,
     bottom: 0,
--- client/app/puzzles/candytile/components/candy-tiles/tiles/hooks/useTileInteraction.ts
+++ client/app/puzzles/candytile/components/candy-tiles/tiles/hooks/useTileInteraction.ts
@@ -1,24 +1,20 @@
 import { levelItemsState } from "../../../../store/levelItems";
 import { useEffect } from "react";
-import { View } from "react-native";
 import { useRecoilValue } from "recoil";
 
 const ALLOWED_ITEM_TYPES = ["Candy", "SuperCandy", "Chocolate"];
-export default (index: number, tileElementState: React.RefObject<View>) => {
+export default (index: number, tileElementState: HTMLElement) => {
   const levelItems = useRecoilValue(levelItemsState);
 
   useEffect(() => {
-    tileElementState.current && validateItemType();
+    tileElementState && validateItemType();
   }, [levelItems, tileElementState]);
 
   const validateItemType = () => {
     const type = levelItems[index]?.type;
     const allowedType = ALLOWED_ITEM_TYPES.includes(type || "");
 
-    if (allowedType) {
-      tileElementState.current?.setNativeProps({ accessibilityLabel: "Tile" });
-    } else {
-      tileElementState.current?.setNativeProps({ accessibilityLabel: "Not a tile" });
-    }
+    allowedType ? tileElementState.setAttribute("data-tile", "") : tileElementState.removeAttribute("data-tile");
+    tileElementState.style.cursor = allowedType ? "default" : "not-allowed";
   };
 };
